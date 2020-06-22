e244dad85a350e7e4043b06efd43ca321549c5a9
Maxim Shiryaev <maksim155555@gmail.com>
2020-06-22 19:18:08 +0300

    testing in progress

--- slack_integration/tests/test_views.py
+++ slack_integration/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from slack_integration.models import *
+
 
 class AppListViewTest(TestCase):
     fixtures = ('dump.json',)
@@ -21,6 +23,28 @@ class AppListViewTest(TestCase):
 class CreateAppViewTest(TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('app-registration')
+    login_url = reverse('login')
+
+    def test_login_required(self):
+        response = self.client.get(self.tested_url)
+        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
+                                                       self.tested_url))
+
+    def test_logged_in_not_admin(self):
+        self.client.login(username='dev', password='dev')
+        response = self.client.get(self.tested_url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_logged_in_admin(self):
+        self.client.login(username='admin', password='admin')
+        response = self.client.get(self.tested_url)
+        self.assertEqual(response.status_code, 200)
+
+
+class UpdateAppViewTest(TestCase):
+    fixtures = ('dump.json',)
+    tested_url = reverse('app-update',
+                         args=(SlackApplication.objects.first().pk,))
     login_url = reverse('login')
 
     def test_login_required(self):
