9356779d4d159730bde650139c6c1e9620b5c57a
Maxim Shiryaev <maksim155555@gmail.com>
2020-06-24 11:37:41 +0300

    Fixed tests according to the DRY principle

--- slack_integration/tests/test_views.py
+++ slack_integration/tests/test_views.py
@@ -1,352 +1,106 @@
 from django.test import TestCase
-from django.urls import reverse
 from slack_integration.models import (SlackApplication, Template, ActionsBlock)
+from .mixins import *
 
 
-class AppListViewTest(TestCase):
+class AppListViewTest(LoginRequiredTestMixin, LoggedInTestMixin,
+                      TemplateUsedTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('app-list')
-    login_url = reverse('login')
     template_name = 'slack_integration/list/app.html'
 
-    def test_login_required(self):
-        response = self.client.get(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        # Checks the logged in user does not redirect to the login page
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class AppDetailViewTest(TestCase):
+class AppDetailViewTest(LoginRequiredTestMixin, LoggedInTestMixin,
+                        TemplateUsedTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('app-detail',
                          args=(SlackApplication.objects.first().pk,))
-    login_url = reverse('login')
     template_name = 'slack_integration/detail/app.html'
 
-    def test_login_required(self):
-        response = self.client.get(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        # Checks the logged in user does not redirect to the login page
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class CreateAppViewTest(TestCase):
+class CreateAppViewTest(LoginRequiredTestMixin, TemplateUsedTestMixin,
+                        LoggedInAdminAndNotAdminTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('app-registration')
-    login_url = reverse('login')
     template_name = 'slack_integration/create/app.html'
 
-    def test_login_required(self):
-        response = self.client.get(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in_not_admin(self):
-        self.client.login(username='dev', password='dev')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_logged_in_admin(self):
-        self.client.login(username='admin', password='admin')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class UpdateAppViewTest(TestCase):
+class UpdateAppViewTest(LoginRequiredTestMixin, TemplateUsedTestMixin,
+                        LoggedInAdminAndNotAdminTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('app-update',
                          args=(SlackApplication.objects.first().pk,))
-    login_url = reverse('login')
     template_name = 'slack_integration/update/app.html'
 
-    def test_login_required(self):
-        response = self.client.get(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in_not_admin(self):
-        self.client.login(username='dev', password='dev')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_logged_in_admin(self):
-        self.client.login(username='admin', password='admin')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class DeleteAppViewTest(TestCase):
+class DeleteAppViewTest(LoginRequiredTestMixin, DeleteTestMixin, TestCase):
     fixtures = ('dump.json',)
-    object = SlackApplication.objects.first()
+    model = SlackApplication
+    object = model.objects.first()
     tested_url = reverse('app-delete',
                          args=(object.pk,))
-    login_url = reverse('login')
-
-    def test_login_required(self):
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
-
-    def test_admin_can_delete_post(self):
-        self.client.login(username='admin', password='admin')
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '/')
-
-    def test_not_admin_cannot_delete_post(self):
-        self.client.login(username='dev', password='dev')
-        response = self.client.post(self.tested_url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_obj_is_deleted_post(self):
-        self.client.login(username='admin', password='admin')
-        self.client.post(self.tested_url)
-        # Check that object has been deleted.
-        self.assertFalse(self.object in SlackApplication.objects.all())
 
 
-class CreateTemplateViewTest(TestCase):
+class CreateTemplateViewTest(LoginRequiredTestMixin, TemplateUsedTestMixin,
+                             LoggedInAdminAndNotAdminTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('template-create',
                          args=(SlackApplication.objects.first().pk,))
-    login_url = reverse('login')
     template_name = 'slack_integration/create/template.html'
 
-    def test_login_required(self):
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in_not_admin(self):
-        self.client.login(username='dev', password='dev')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_logged_in_admin(self):
-        self.client.login(username='admin', password='admin')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class TemplateDetailViewTest(TestCase):
+class TemplateDetailViewTest(LoginRequiredTestMixin, LoggedInTestMixin,
+                             TemplateUsedTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('template-detail',
                          args=(Template.objects.first().pk,))
-    login_url = reverse('login')
     template_name = 'slack_integration/detail/template.html'
 
-    def test_login_required(self):
-        response = self.client.get(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        # Checks the logged in user does not redirect to the login page
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class UpdateTemplateViewTest(TestCase):
+class UpdateTemplateViewTest(LoginRequiredTestMixin, TemplateUsedTestMixin,
+                             LoggedInAdminAndNotAdminTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('template-update',
                          args=(Template.objects.first().pk,))
-    login_url = reverse('login')
     template_name = 'slack_integration/update/template.html'
 
-    def test_login_required(self):
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in_not_admin(self):
-        self.client.login(username='dev', password='dev')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_logged_in_admin(self):
-        self.client.login(username='admin', password='admin')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class DeleteTemplateViewTest(TestCase):
+class DeleteTemplateViewTest(LoginRequiredTestMixin, DeleteTestMixin,
+                             TestCase):
     fixtures = ('dump.json',)
-    object = Template.objects.first()
+    model = Template
+    object = model.objects.first()
     tested_url = reverse('template-delete',
                          args=(object.pk,))
-    login_url = reverse('login')
-
-    def test_login_required(self):
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
-
-    def test_admin_can_delete_post(self):
-        self.client.login(username='admin', password='admin')
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '/')
-
-    def test_not_admin_cannot_delete_post(self):
-        self.client.login(username='dev', password='dev')
-        response = self.client.post(self.tested_url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_obj_is_deleted_post(self):
-        self.client.login(username='admin', password='admin')
-        self.client.post(self.tested_url)
-        # Check that object has been deleted.
-        self.assertFalse(self.object in Template.objects.all())
 
 
-class CreateActionsBlockViewTest(TestCase):
+class CreateActionsBlockViewTest(LoginRequiredTestMixin, TemplateUsedTestMixin,
+                                 LoggedInAdminAndNotAdminTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('actions-block-create',
                          args=(ActionsBlock.objects.first().pk,))
-    login_url = reverse('login')
     template_name = 'slack_integration/create/actions_block.html'
 
-    def test_login_required(self):
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in_not_admin(self):
-        self.client.login(username='dev', password='dev')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_logged_in_admin(self):
-        self.client.login(username='admin', password='admin')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class ActionsBlockDetailViewTest(TestCase):
+class ActionsBlockDetailViewTest(LoginRequiredTestMixin, LoggedInTestMixin,
+                                 TemplateUsedTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('actions-block-detail',
                          args=(ActionsBlock.objects.first().pk,))
-    login_url = reverse('login')
     template_name = 'slack_integration/detail/actions_block.html'
 
-    def test_login_required(self):
-        response = self.client.get(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        # Checks the logged in user does not redirect to the login page
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class UpdateActionsBlockViewTest(TestCase):
+class UpdateActionsBlockViewTest(LoginRequiredTestMixin, TemplateUsedTestMixin,
+                                 LoggedInAdminAndNotAdminTestMixin, TestCase):
     fixtures = ('dump.json',)
     tested_url = reverse('actions-block-update',
                          args=(ActionsBlock.objects.first().pk,))
-    login_url = reverse('login')
     template_name = 'slack_integration/update/actions_block.html'
 
-    def test_login_required(self):
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
 
-    def test_logged_in_not_admin(self):
-        self.client.login(username='dev', password='dev')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_logged_in_admin(self):
-        self.client.login(username='admin', password='admin')
-        response = self.client.get(self.tested_url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_used(self):
-        self.client.login(username='adm', password='adm')
-        response = self.client.get(self.tested_url)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class DeleteActionsBlockViewTest(TestCase):
+class DeleteActionsBlockViewTest(LoginRequiredTestMixin, TestCase):
     fixtures = ('dump.json',)
-    object = ActionsBlock.objects.first()
+    model = ActionsBlock
+    object = model.objects.first()
     tested_url = reverse('actions-block-delete',
                          args=(object.pk,))
-    login_url = reverse('login')
-
-    def test_login_required(self):
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '%s?next=%s' % (self.login_url,
-                                                       self.tested_url))
-
-    def test_admin_can_delete_post(self):
-        self.client.login(username='admin', password='admin')
-        response = self.client.post(self.tested_url)
-        self.assertRedirects(response, '/')
-
-    def test_not_admin_cannot_delete_post(self):
-        self.client.login(username='dev', password='dev')
-        response = self.client.post(self.tested_url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_obj_is_deleted_post(self):
-        self.client.login(username='admin', password='admin')
-        self.client.post(self.tested_url)
-        # Check that object has been deleted.
-        self.assertFalse(self.object in ActionsBlock.objects.all())
